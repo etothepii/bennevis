9be0661f2bf24011aa0e26f373477ed8429dd906
James Robinson <github@epii.co.uk>
2015-03-22 20:04:59 +0000

    Shell to begin loading GPX file

---
+++ src/test/java/uk/co/epii/bennevis/TestData.java
@@ -0,0 +1,14 @@
+package uk.co.epii.bennevis;
+
+/**
+ * User: James Robinson
+ * Date: 22/03/2015
+ * Time: 20:01
+ */
+public class TestData {
+
+  public static Object[][] loadCsv(String name) {
+    return null;
+  }
+
+}
---
+++ src/test/java/uk/co/epii/bennevis/gpx/GPXLoaderTests.java
@@ -0,0 +1,9 @@
+package uk.co.epii.bennevis.gpx;
+
+/**
+ * User: James Robinson
+ * Date: 22/03/2015
+ * Time: 20:03
+ */
+public class GPXLoaderTests {
+}
