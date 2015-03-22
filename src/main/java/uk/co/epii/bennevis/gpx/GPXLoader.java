fda97c9dc67b2dc3734fef90f47700516e4ed70b
James Robinson <github@epii.co.uk>
2015-03-22 23:18:24 +0000

    Loads GPX stream into DOM

--- src/main/java/uk/co/epii/bennevis/gpx/GPXLoader.java
+++ src/main/java/uk/co/epii/bennevis/gpx/GPXLoader.java
@@ -1,7 +1,16 @@
 package uk.co.epii.bennevis.gpx;
 
+import org.w3c.dom.Document;
+import org.xml.sax.EntityResolver;
+import org.xml.sax.InputSource;
+import org.xml.sax.SAXException;
+
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
 import java.awt.geom.Point2D;
+import java.io.IOException;
 import java.io.InputStream;
+import java.io.StringReader;
 
 /**
  * User: James Robinson
@@ -10,8 +19,31 @@ import java.io.InputStream;
  */
 public class GPXLoader {
 
-  public void loadFile(InputStream inputStream) {
+  private Document document;
 
+  public void loadFile(InputStream inputStream) {
+    try {
+      DocumentBuilderFactory dbf = DocumentBuilderFactory.newInstance();
+
+      dbf.setValidating(false);
+      dbf.setIgnoringComments(false);
+      dbf.setIgnoringElementContentWhitespace(true);
+      dbf.setNamespaceAware(true);
+
+      DocumentBuilder db = null;
+      db = dbf.newDocumentBuilder();
+      db.setEntityResolver(new EntityResolver() {
+        @Override
+        public InputSource resolveEntity(String publicId, String systemId) throws SAXException, IOException {
+          return new InputSource(new StringReader(""));
+        }
+      });
+
+      document = db.parse(inputStream);
+    }
+    catch (Exception e) {
+      throw new RuntimeException(e);
+    }
   }
 
   public Point2D.Float[] getPoints() {
