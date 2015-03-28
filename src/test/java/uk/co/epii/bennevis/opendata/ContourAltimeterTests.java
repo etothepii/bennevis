60bed928bf013c8437ba836e84e35f61b9c273d3
James Robinson <github@epii.co.uk>
2015-03-28 23:14:41 +0000

    Loaded results from altitude in to text file to use a base test

--- src/test/java/uk/co/epii/bennevis/opendata/ContourAltimeterTests.java
+++ src/test/java/uk/co/epii/bennevis/opendata/ContourAltimeterTests.java
@@ -15,32 +15,15 @@ import static org.junit.Assert.assertEquals;
  */
 public class ContourAltimeterTests {
 
-  private static IAltimeter altimeter;
-
-  @BeforeClass
-  public static void beforeClass() {
-    altimeter = new ContourAltimeter();
-  }
-
-  @Test
-  public void canCalculateAltitude1() {
-    OSRef osRef = new OSRef(271719d, 149514d);
-    double expected = 170d;
-    double result = altimeter.getAltitude(osRef);
-    assertEquals(expected, result, 1d);
-  }
-
   @Test
   public void canCalculateAltitudes() {
     IAltimeter altimeter = new ContourAltimeter();
     OSRef[] osrefs = GPXLoaderTests.loadPointsFromCSVData("ExampleWalk.csv");
     double[] expected = AltitudeLoader.loadAltitudesFromCSVData("ExampleWalkContourAltitude.csv");
-    double result = altimeter.getAltitude(new OSRef(271719d, 149514d));
     assertEquals("size of array", expected.length, osrefs.length);
     for (int i = 0; i < expected.length; i++) {
-      result = altimeter.getAltitude(osrefs[i]);
-      System.out.println(osrefs[i].getEasting() + "," + osrefs[i].getNorthing() + "," + result);
-      //assertEquals("Point[" + i + "] altitude", expected[i], result, 0.00001);
+      double result = altimeter.getAltitude(osrefs[i]);
+      assertEquals("Point[" + i + "] altitude", expected[i], result, 0.00001);
     }
   }
 
